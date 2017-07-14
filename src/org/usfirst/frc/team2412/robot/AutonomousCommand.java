01a94f4d6525b4848e304c3ea1b8cfed520b70cf
Timothy Eng <timothyeng36@gmail.com>
2017-07-13 18:05:53 -0700

    Changed AutonomousCommand to a group of all commands to be run in autonomous.

--- src/org/usfirst/frc/team2412/robot/AutonomousCommand.java
+++ src/org/usfirst/frc/team2412/robot/AutonomousCommand.java
@@ -1,85 +1,30 @@
 package org.usfirst.frc.team2412.robot;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /*
- * This class represents a command to be run in autonomous, such as driving forward using motion profiling.
+ * This class represents a group of commands to be run in autonomous.
  */
 
-public class AutonomousCommand extends Command {
-	private boolean isRunning;
-	private int commandStage;
+public class AutonomousCommand extends CommandGroup {
 	
-	/** TODO Possibly change this constructor? */
-	public AutonomousCommand(int _commandStage) {
-		isRunning = false;
-		this.commandStage = _commandStage;
-	}
-
 	/**
-	 * Called periodically when the command is running.
+	 * Creates an AutonomousCommand from an ArrayList of AutonomousStage objects.
+	 * Assumes that commands have already been added to each AutonomousStage (and have been sent to the SmartDashboard).
 	 */
-	protected void runPeriodic() {
-
-	}
-
-	/**
-	 * Called if this command should be running.
-	 */
-	protected boolean shouldBeRunning() {
-		return commandStage == AutonomousCommandManager.currentStage;
-	}
-
-	/**
-	 * Called when the command first starts.
-	 */
-	protected void initialize() {
-		isRunning = true;
-	}
-
-	/**
-	 * Called by other classes that use AutonomousCommand.
-	 * This method determines when the other methods should be called.
-	 */
-	protected void execute() {
-		if(shouldBeRunning()) {
-			if(isRunning) {
-				if(isFinished()) {
-					//Command's about to stop.
-					AutonomousCommandManager.currentStage++; //Move on to the next stage
-					end();
-				} else {
-					//Command's currently running.
-					runPeriodic();
-				}
-			} else {
-				//Command's about to start.
-				initialize();
+	public AutonomousCommand(ArrayList<AutonomousStage> stages) {
+		for(AutonomousStage stage : stages) {
+			Command selectedCommand = stage.getSelected();
+			if(selectedCommand == null) {
+				System.err.println("No stage was selected!");
+				continue;
 			}
+			addSequential(selectedCommand);
 		}
 	}
-
-	/**
-	 * Determines if the command is finished.
-	 * This method should be overriden by any subclasses.
-	 */
-	protected boolean isFinished() {
-		return false;
-	}
-
-	/**
-	 * Called when the command ends.
-	 */
-	protected void end() {
-		isRunning = false;
-	}
-
-
-	/**
-	 * Called if the command has been interrupted.
-	 */
-
-	protected void interrupted() {
-		//Do nothing...for now.
-	}
+	
+	
 }
